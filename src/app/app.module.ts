8072acb1016bbea55365e4f844f06fb2382793a4
Mila Hristova <hristova.mila@abv.bg>
2019-01-10 12:35:42 +0200

    Non-responsive views are done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    AboutComponent,
+    ActivitiesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +23,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+ 
+}
